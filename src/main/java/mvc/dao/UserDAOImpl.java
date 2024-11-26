3be864ec856840ef599659cefbf6f676e067c599
Bydanoff <erzhan.vst@gmail.com>
2024-11-26 20:11:14 +0300

    SpringBoot!!!

--- src/main/java/mvc/dao/UserDAOImpl.java
+++ src/main/java/mvc/dao/UserDAOImpl.java
@@ -2,9 +2,9 @@ package mvc.dao;
 
 import mvc.model.User;
 import org.springframework.stereotype.Repository;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import java.util.List;
 
 @Repository
--- src/main/java/mvc/repository/UserRepositoryImpl.java
+++ src/main/java/mvc/repository/UserRepositoryImpl.java
@@ -2,8 +2,8 @@ package mvc.repository;
 
 import mvc.model.User;
 import org.springframework.stereotype.Repository;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
